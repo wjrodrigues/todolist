dea993efac5a60d90b9e874588a243e72bcf92dd
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-02-29 14:36:50 -0300

    chore: create environment

---
+++ configs/environment.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"github.com/spf13/viper"
+)
+
+var values *env
+
+type env struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+}
+
+func LoadEnv(path, file string) (env, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(file)
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return env{}, err
+	}
+
+	err = viper.Unmarshal(&values)
+
+	return *values, err
+}
---
+++ configs/environment_test.go
@@ -0,0 +1,24 @@
+package configs
+
+import (
+	"testing"
+	"todolist/pkg/test"
+
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/lib/pq"
+)
+
+func TestFileExists(t *testing.T) {
+	values, err := LoadEnv(".", test.FilePath(".env"))
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, values)
+}
+
+func TestFileNotExists(t *testing.T) {
+	values, err := LoadEnv(".", "any")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, values, env{})
+}
