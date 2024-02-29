6d6892ab2531d5b7b448fe03ee208ea4e03025af
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-02-29 14:35:30 -0300

    chore: add help for load .env on tests

---
+++ pkg/test/environment.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func FilePath(fileName string) string {
+	absolutePath, _ := os.Getwd()
+	basePath := strings.Split(absolutePath, "app")[0]
+
+	return fmt.Sprintf("%s/app/%s", basePath, fileName)
+}
