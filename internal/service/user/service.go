a1efb1b9f88070721e4a22e6d2b557ecd9cabfc7
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-18 10:14:04 -0300

    User service (#17)
    * chore: update interface Database
    
    * chore: create service for user

---
+++ internal/service/user/service.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"todolist/infra/database"
+	entity "todolist/internal/entity/user"
+)
+
+type IUserService interface {
+	Create(user *entity.User) error
+	FindByEmailOrId(id, email string) (*entity.User, error)
+}
+
+type UserService struct {
+	database database.IUser
+}
+
+func NewUserService(database database.IUser) *UserService {
+	return &UserService{
+		database: database,
+	}
+}
+
+func (i *UserService) Create(list *entity.User) error {
+	if err := i.database.Create(list); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *UserService) Find(value string) (*entity.User, error) {
+	result, err := i.database.FindByEmailOrId(value, value)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
---
+++ internal/service/user/service_test.go
@@ -0,0 +1,55 @@
+package user
+
+import (
+	"testing"
+
+	userDb "todolist/infra/database/pg/user"
+	userEntity "todolist/internal/entity/user"
+	"todolist/pkg/test"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAndFindUsertWithSuccess(t *testing.T) {
+	userDB := userDb.NewUserDB(test.Conn(t))
+	user, _ := userEntity.NewUser("Pedro", "service_create_list@email.com", "123")
+
+	service := NewUserService(userDB)
+
+	err := service.Create(user)
+	assert.Nil(t, err)
+
+	result, err := service.Find(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, result.Name, user.Name)
+
+	t.Cleanup(func() {
+		userDB.Delete(user.Email)
+	})
+}
+
+func TestCreateUsertWithFail(t *testing.T) {
+	userDB := userDb.NewUserDB(test.Conn(t))
+	user, _ := userEntity.NewUser("Pedro", "service_create_list@email.com", "123")
+
+	service := NewUserService(userDB)
+
+	service.Create(user)
+	err := service.Create(user)
+	assert.Error(t, err)
+
+	t.Cleanup(func() {
+		userDB.Delete(user.Email)
+	})
+}
+
+func TestFidUsertWithFail(t *testing.T) {
+	userDB := userDb.NewUserDB(test.Conn(t))
+	userDB.DB.Close()
+	service := NewUserService(userDB)
+
+	response, err := service.Find("any")
+
+	assert.Nil(t, response)
+	assert.Error(t, err)
+}
