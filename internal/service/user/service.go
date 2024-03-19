d0d486fe9c1df3ca3682c0afb778b6f7e3b942bb
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-19 11:01:46 -0300

    chore: add method auth on user/service

--- internal/service/user/service.go
+++ internal/service/user/service.go
@@ -1,13 +1,17 @@
 package user
 
 import (
+	"errors"
 	"todolist/infra/database"
 	entity "todolist/internal/entity/user"
 )
 
+const InvalidAuth = "email or password are invalid"
+
 type IUserService interface {
 	Create(user *entity.User) error
 	Find(value string) (*entity.User, error)
+	Auth(email, password string) (string, error)
 }
 
 type UserService struct {
@@ -20,20 +24,36 @@ func NewUserService(database database.IUser) *UserService {
 	}
 }
 
-func (i *UserService) Create(list *entity.User) error {
-	if err := i.database.Create(list); err != nil {
+func (u *UserService) Create(user *entity.User) error {
+	if err := u.database.Create(user); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (i *UserService) Find(value string) (*entity.User, error) {
-	result, err := i.database.FindByEmailOrId(value, value)
+func (u *UserService) Find(value string) (*entity.User, error) {
+	result, err := u.database.FindByEmailOrId(value, value)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return result, nil
+}
+
+func (u *UserService) Auth(email, password string) (string, error) {
+	result, _ := u.database.FindByEmailOrId("", email)
+
+	if result == nil {
+		return "", errors.New(InvalidAuth)
+	}
+
+	valid := result.ValidatePassword(password)
+
+	if !valid {
+		return "", errors.New(InvalidAuth)
+	}
+
+	return result.ID.String(), nil
 }
