df77b0583a9138306c92edac298fe29099136944
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-19 11:02:44 -0300

    chore: create auth on user/handler

--- infra/webserver/handlers/user_test.go
+++ infra/webserver/handlers/user_test.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"todolist/infra/database/pg/user"
+	entity "todolist/internal/entity/user"
+
 	service "todolist/internal/service/user"
 	"todolist/pkg/test"
 
@@ -90,4 +93,70 @@ func TestNotCreateWithErrorOnService(t *testing.T) {
 	got := response.Body.String()
 	assert.Equal(t, got, "{\"message\":\"Server error, please try again\"}\n")
 	assert.Equal(t, response.Code, http.StatusInternalServerError)
+}
+
+func TestAuthWithSuccess(t *testing.T) {
+	userDb := user.NewUserDB(test.Conn(t))
+	user, _ := entity.NewUser("Maria", "auth@email.com", "123")
+	userDb.Create(user)
+
+	service := service.NewUserService(userDb)
+	handler := NewUserHandler(service)
+
+	body := bytes.Buffer{}
+	body.Write([]byte("{\"email\": \"auth@email.com\", \"password\": \"123\"}"))
+
+	request, _ := http.NewRequest(http.MethodPost, "/api/user/auth", &body)
+	response := httptest.NewRecorder()
+
+	handler.Auth(response, request, "123")
+
+	got := response.Body.String()
+
+	assert.True(t, strings.Contains(got, "access_token"))
+	assert.Equal(t, response.Code, http.StatusOK)
+
+	t.Cleanup(func() {
+		userDb.Delete("auth@email.com")
+	})
+}
+
+func TestAuthWithInvalidUser(t *testing.T) {
+	userDb := user.NewUserDB(test.Conn(t))
+
+	service := service.NewUserService(userDb)
+	handler := NewUserHandler(service)
+
+	body := bytes.Buffer{}
+	body.Write([]byte("{\"email\": \"auth@email.com\", \"password\": \"123\"}"))
+
+	request, _ := http.NewRequest(http.MethodPost, "/api/user/auth", &body)
+	response := httptest.NewRecorder()
+
+	handler.Auth(response, request, "123")
+
+	got := response.Body.String()
+
+	assert.Equal(t, got, "{\"message\":\"email or password are invalid\"}\n")
+	assert.Equal(t, response.Code, http.StatusUnauthorized)
+}
+
+func TestAuthWithInvalidBody(t *testing.T) {
+	userDb := user.NewUserDB(test.Conn(t))
+
+	service := service.NewUserService(userDb)
+	handler := NewUserHandler(service)
+
+	body := bytes.Buffer{}
+	body.Write([]byte("{"))
+
+	request, _ := http.NewRequest(http.MethodPost, "/api/user/auth", &body)
+	response := httptest.NewRecorder()
+
+	handler.Auth(response, request, "/")
+
+	got := response.Body.String()
+
+	assert.Equal(t, got, "")
+	assert.Equal(t, response.Code, http.StatusBadRequest)
 }
