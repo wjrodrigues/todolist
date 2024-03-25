6d1932fe3c66e20178f5c478dadea344d396ac00
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-25 08:56:27 -0300

    chore: remove unnecessary files

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline bg-blue-300">This is an about page</h1>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
