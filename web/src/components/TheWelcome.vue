a40da696bb5ec29d06929c9ad293b69ae02fd119
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-25 09:05:11 -0300

    Front structure (#20)
    * chore: create structure web
    
    * chore: add tailwindcss dependency
    
    * chore: config tailwindcss
    
    * chore: update dependency
    
    * chore: update makefile
    
    * chore: remove unnecessary files
    
    * chore: add vitest dependency

---
+++ web/src/components/TheWelcome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline bg-red-300">Welcome</h1>
+  </div>
+</template>
---
+++ web/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1 class="text-3xl font-bold underline bg-blue-300">This is an about page</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
