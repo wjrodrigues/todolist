0bb08e1137764cda4fa890d265600f881d74c894
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-04-02 13:57:14 -0300

    chore: config alias and server

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
   server: {
     port: 8080,
-    host: '0.0.0.0'
-  }
-})
+    host: "0.0.0.0",
+  },
+});
