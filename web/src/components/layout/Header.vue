6bae894878d1c69d3e0f950a4d2eccf323921390
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-05-04 17:27:11 -0300

    feat: add Header on layout

---
+++ web/src/components/layout/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar color="#6A1B9A" density="compact" :elevation="0">
+    <v-app-bar-title>Todolist</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn icon="mdi-magnify"></v-btn>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+</script>
